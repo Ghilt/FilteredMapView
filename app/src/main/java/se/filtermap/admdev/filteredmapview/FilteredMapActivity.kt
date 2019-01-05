d38de8d829d78d238a2bbe407d25cd918be1cadc
Adam Nilsson <adam.n.a3@gmail.com>
2019-01-05 16:16:18 +0100

    Add bottom sheet

--- app/src/main/java/se/filtermap/admdev/filteredmapview/FilteredMapActivity.kt
+++ app/src/main/java/se/filtermap/admdev/filteredmapview/FilteredMapActivity.kt
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -37,7 +38,7 @@ class FilteredMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
