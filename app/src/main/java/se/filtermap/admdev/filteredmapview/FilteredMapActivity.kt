d689210ad1d13155cfec2df82105773838304554
Adam Nilsson <adam.n.a3@gmail.com>
2019-01-05 22:29:06 +0100

    Experiment with google maps markers

--- app/src/main/java/se/filtermap/admdev/filteredmapview/FilteredMapActivity.kt
+++ app/src/main/java/se/filtermap/admdev/filteredmapview/FilteredMapActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomSheetBehavior
 import android.support.v4.widget.NestedScrollView
+import android.util.Log
+import android.widget.SeekBar
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,8 +13,10 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_filtered_map.*
+import kotlinx.android.synthetic.main.settings_bottom_sheet_content.*
 
 class FilteredMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -29,7 +33,23 @@ class FilteredMapActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         mBottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet)
+
+        bottom_sheet_seek_bar.setOnSeekBarChangeListener( object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(v: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.d("temp", "progress $progress")
+                marker.position = LatLng(marker.position.latitude, marker.position.longitude + 0.2)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
     }
+
+    private lateinit var marker: Marker
 
     /**
      * Manipulates the map once available.
@@ -44,8 +64,11 @@ class FilteredMapActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
+        mMap.applyFilter(MapFilter(10000, 100000))
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
+        val markerTest = MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+        marker = mMap.addMarker(markerTest)
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
+
